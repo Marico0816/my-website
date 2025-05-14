9a54a82cb35fdbabb1ba0fee2b77e1a6e9717fc6
Marico0816 <shengzhe05@outlook.com>
2025-05-14 16:30:33 -0400

    更新内容

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function changeColor() {
-  const colors = ['#ffadad', '#ffd6a5', '#fdffb6', '#caffbf', '#9bf6ff'];
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
-  document.body.style.backgroundColor = randomColor;
+  document.body.style.backgroundColor =
+    document.body.style.backgroundColor === "lightgreen"
+      ? "white"
+      : "lightgreen";
 }
