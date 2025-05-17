b7a494fa67fb71350c6c66c111969e08820bb29e
Marico0816 <shengzhe05@outlook.com>
2025-05-17 18:28:01 -0400

    更新 CSS 和 JS

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ function changeColor() {
       : "lightgreen";
 }
 
+const startInput = document.getElementById("start-date");
+const endInput = document.getElementById("end-date");
+
+flatpickr(startInput, {
+  dateFormat: "Y-m-d",
+  locale: "en",
+  onChange: function (selectedDates) {
+    endPicker.set("minDate", selectedDates[0]);
+  }
+});
+
+const endPicker = flatpickr(endInput, {
+  dateFormat: "Y-m-d",
+  locale: "en"
+});
